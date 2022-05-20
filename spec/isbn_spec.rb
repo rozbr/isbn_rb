a9a0760e7a445a2d9d59c0bd056918f28f606d8e
Rosemilson <rozbr96@gmail.com>
2022-05-20 15:32:49 -0300

    Add contexts to ensure input correctness

--- spec/isbn_spec.rb
+++ spec/isbn_spec.rb
@@ -2,5 +2,33 @@
 require_relative "../src/isbn"
 
 
-RSpec.describe ISBN do
+describe ISBN do
+  describe "ensure_correctness_input," do
+    context "when passing a non string/number as paramer," do
+      it "should raise argument error" do
+        expect {ISBN.ensure_correctness_input true}.to raise_error ArgumentError
+        expect {ISBN.ensure_correctness_input false}.to raise_error ArgumentError
+        expect {ISBN.ensure_correctness_input nil}.to raise_error ArgumentError
+      end
+    end
+
+    context "when passing an invalid string," do
+      it "should raise argument error if length is different than 12 and 13" do
+        expect {ISBN.ensure_correctness_input "97821234568"}.to raise_error ArgumentError
+        expect {ISBN.ensure_correctness_input "97821234568034"}.to raise_error ArgumentError
+      end
+
+      it "should raise argument error if any non numeric digit is found" do
+        expect {ISBN.ensure_correctness_input "97821234568O3"}.to raise_error ArgumentError
+        expect {ISBN.ensure_correctness_input "9782123A5680"}.to raise_error ArgumentError
+      end
+    end
+
+    context "when passing a valid string," do
+      it "should not raise errors" do
+        expect {ISBN.ensure_correctness_input "9782123456803"}.not_to raise_error
+        expect {ISBN.ensure_correctness_input "012345678901"}.not_to raise_error
+      end
+    end
+  end
 end
--- src/isbn.rb
+++ src/isbn.rb
@@ -1,3 +1,42 @@
 
-class ISBN
+module ISBN
+  def self.check_value_digits value
+    non_numeric_index = value =~ /[^0-9]/
+
+    unless non_numeric_index.class == NilClass
+      raise ArgumentError.new %{
+        Expected a numeric string but a non numeric digit, #{value[non_numeric_index]},
+        was found in the index position #{non_numeric_index}
+      }
+    end
+  end
+
+  def self.check_value_length value
+    unless [12, 13].include? value.size
+      raise ArgumentError.new %{
+        Expected a string with 12 or 13 digits
+        but #{value.size} digits were given
+      }
+    end
+  end
+
+  def self.check_value_type value
+    unless [Integer, String].include? value.class
+      raise ArgumentError.new %{
+        Expected a String/Number but #{value.class} was given
+      }
+    end
+  end
+
+  def self.ensure_correctness_input value
+    ISBN.check_value_type value
+
+    value = value.to_s
+
+    ISBN.check_value_length value
+
+    ISBN.check_value_digits value
+
+    value
+  end
 end
