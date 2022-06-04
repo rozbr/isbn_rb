a1df2ca770697954bca5abf182d0e27db1dd3d11
Rosemilson <rozbr96@gmail.com>
2022-06-04 00:50:40 -0300

    Remove line breaks from error messages

--- src/isbn.rb
+++ src/isbn.rb
@@ -4,27 +4,19 @@ module ISBN
     non_numeric_index = value =~ /[^0-9]/
 
     unless non_numeric_index.class == NilClass
-      raise ArgumentError.new %{
-        Expected a numeric string but a non numeric digit, #{value[non_numeric_index]},
-        was found in the index position #{non_numeric_index}
-      }
+      raise ArgumentError.new "Expected a numeric string but a non numeric digit, #{value[non_numeric_index]}, was found in the index position #{non_numeric_index}"
     end
   end
 
   def self.check_value_length value
     unless [12, 13].include? value.size
-      raise ArgumentError.new %{
-        Expected a string with 12 or 13 digits
-        but #{value.size} digits were given
-      }
+      raise ArgumentError.new "Expected a string with 12 or 13 digits but #{value.size} digits were given"
     end
   end
 
   def self.check_value_type value
     unless [Integer, String].include? value.class
-      raise ArgumentError.new %{
-        Expected a String/Number but #{value.class} was given
-      }
+      raise ArgumentError.new "Expected a String/Number but #{value.class} was given"
     end
   end
 
